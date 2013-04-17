dc3caa573cd2663303936695b94d6a3a819ead24
frankfeng <frankfeng@dnspod.com>
2013-04-17 15:05:59 +0800

    fix send.sh ip

--- src/send.sh
+++ src/send.sh
@@ -1,5 +1,14 @@
-tsar  -i 9  --cpu --io --mem --udp --tcp --traffic  --tcpx --load --pcsw    -l | while read line
+#!/bin/sh
+
+if [ $# != 2 ]
+then
+      echo "Usage: tsar-udp server_ip server_port"
+else
+
+tsar  -i 10  --cpu --io --mem --udp --tcp --traffic  --tcpx --load --pcsw    -l | while read line
 do
-  echo $line |  nc -u -w 1 172.4.0.23 8125
+  echo $line |  nc -u -w 0 $1 $2
   echo $line 
 done
+
+fi
