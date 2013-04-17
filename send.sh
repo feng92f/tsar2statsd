c9e1ee977e8072a3a6470511b1e739042a50689a
frankfeng <frankfeng@dnspod.com>
2013-04-17 14:31:35 +0800

    add send

--- send.sh
+++ send.sh
@@ -1,4 +1,4 @@
-tsar  -i 9  --cpu --io --mem --udp --tcp --traffic  --tcpx --load -l | while read line
+tsar  -i 9  --cpu --io --mem --udp --tcp --traffic  --tcpx --load --pcsw    -l | while read line
 do
   echo $line |  nc -u -w 1 172.4.0.23 8125
   echo $line 
